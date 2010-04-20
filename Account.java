f7556c936fd1df60c8b46961e1797f4ba9429202
pauledangerously1 <pauledangerously1@d6ec4651-b2ca-d279-b62f-cb9b843d6bd8>
2010-04-20 05:22:22 +0000

    Adding our initial code... (Account.java, Client.java, MailBox.java)

---
+++ Account.java
@@ -0,0 +1,20 @@
+/*
+ * Authors: Paul Tang, Mark Ramasco
+ * CNT4007 Project 2 
+ */
+
+
+//*******************************Account*******************************
+/*
+ * Accounts on the server.
+*/
+ public class Account
+{
+	private   String name;
+	protected String password;
+	
+	public Account()
+	{
+	}	
+	
+}
