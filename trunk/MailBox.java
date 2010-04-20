03b88fa2a23e4e52bcf80ebd482ff79e3e8ede5c
pauledangerously1 <pauledangerously1@d6ec4651-b2ca-d279-b62f-cb9b843d6bd8>
2010-04-20 05:22:22 +0000

    Adding our initial code... (Account.java, Client.java, MailBox.java)

---
+++ trunk/MailBox.java
@@ -0,0 +1,15 @@
+/*
+ * Authors: Paul Tang, Mark Ramasco
+ * CNT4007 Project 2 
+ */
+
+//*******************************MailBox*******************************
+//Data structure for the mailbox
+
+ public class MailBox{
+ 
+ public MailBox(){
+ 
+ }
+ 
+ }
