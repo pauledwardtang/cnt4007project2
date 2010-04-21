017b8399cdb0e781576826a992e31c74a06d04ae
pauledangerously1 <pauledangerously1@d6ec4651-b2ca-d279-b62f-cb9b843d6bd8>
2010-04-21 00:45:05 +0000

    Updated Client.
    Authentication is functional. Check check check it out

--- trunk/src/Client.java
+++ trunk/src/Client.java
@@ -6,6 +6,8 @@ import java.net.*;
 import java.io.*;
 import java.util.Random;
 import java.lang.Math;
+import java.lang.Integer;
+
 
 //*******************************Client*******************************
 //*1 socket between authentication server
@@ -13,12 +15,14 @@ import java.lang.Math;
  public class Client{
 	//---********Variables*********---
 
-	//Enumeration for SMTP State machine
-	private enum SMTP_State { HELO, MAIL, RCPT, DATA, MESSAGE, QUIT, FINISH }
-
+	//Enumeration for SMTP State machine and menu options
+	private enum SMTP_State  { HELO, MAIL, RCPT, DATA, MESSAGE, QUIT, FINISH }
+	private enum menuOptions { SEND, RECEIVE,GO_BACK, EXIT }
+	private enum emailOptions{ SAVE, DELETE, GO_BACK, EXIT }
+	
 	//IP Addresses for the servers
-	String SERVER1_IP = "192.168.2.10";
-	String SERVER2_IP = "127.0.0.1";
+	String SERVER1_IP = "10.128.83.21";
+	String SERVER2_IP = "10.128.83.21";
 
 	private Socket socket, authenticationSocket;
 	private String receiver_email_address, sender_email_address;
@@ -27,14 +31,16 @@ import java.lang.Math;
 	private String MIME;
 	private Random rand;
 	private String temp;
+	private boolean authFlag;
 
 	//Readers Writers
 
 	private BufferedReader in;
 	private PrintWriter out;
 
-	private SMTP_State state;
-
+	private SMTP_State   state;
+	//private menuOptions  menuChoice;
+	private emailOptions emailChoice;
 
 	//---********Constructor*********---
 	/*
@@ -61,14 +67,19 @@ import java.lang.Math;
 				if(Math.round(rand.nextFloat()) == 0) //Set IP to server 1
 				{
 					socket = new Socket(SERVER1_IP, 25);
-					authenticationSocket = new Socket(SERVER1_IP, 4444);
+					System.out.println("making socket");
+					//authenticationSocket = new Socket(SERVER1_IP, 4444);
 				}
 				else
 				{
 					socket = new Socket(SERVER1_IP, 25);
-					authenticationSocket = new Socket(SERVER1_IP, 4444);
+					System.out.println("making socket");
+					//authenticationSocket = new Socket(SERVER1_IP, 4444);
 				}
 					
+				//For testing purposes
+				System.out.println(socket.getRemoteSocketAddress());
+				//System.out.println(in.toString());
 			
 			// Creating readers/writers
 			in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -79,9 +90,6 @@ import java.lang.Math;
 				System.out.println("Error: " + e);
 			}
 			
-			//For testing purposes
-			System.out.println(socket.getLocalAddress());
-			System.out.println(in.toString());
 	}
 
 
@@ -98,29 +106,50 @@ import java.lang.Math;
 	 */
 	void getUserInput(){
 			//Buffered writer for reading input from user
-			String authentication;
+			String userName, authentication;
 			String retryTemp;
 			boolean retry = false;
+			boolean showOptions = true;
 			boolean authFlag = false;
+			boolean hasResponded = false;
+			
 			try
 			{
 				do
 				{
 					BufferedReader writer = new BufferedReader(new InputStreamReader(System.in));
+					String replyCode;
 					
+			//***********Getting username and password********************
 					System.out.print("Username: ");
-					authentication = writer.readLine() + " ";
+					userName = writer.readLine() + " ";
 					
 					System.out.print("Password: ");
-					authentication.concat(writer.readLine());
+					authentication = userName + writer.readLine();
 					System.out.println();
 					
-					authFlag = sendAuthentication(authentication);
-					if(authFlag == false)
+					//Sending Authentication data to server
+					System.out.println("AUTH " + authentication);
+					out.println("AUTH " + authentication);
+					do
+					{
+						replyCode = in.readLine();
+						if(replyCode != null)
+						{
+							hasResponded = true;
+							//System.out.println(replyCode);
+							if(replyCode.substring(0,3).equals("777"))
+							{
+								authFlag = true;
+								System.out.println("Authentication successful!");
+							}
+						}
+					}while(hasResponded == false);
+					
+					if(authFlag == false) //If authentication failed, exit or retry
 					{
 						System.out.println("Authentication error! Exit(e) or Retry(r)?");
 						retryTemp = writer.readLine();
-						
 						if(retryTemp.equals("r"))
 							retry = true;
 						else if(retryTemp.equals("e"))
@@ -129,6 +158,63 @@ import java.lang.Math;
 							System.exit(0);
 						}
 					}
+					else 		//Authentication worked!
+					{
+						int choice;
+						do
+						{
+							System.out.println("****************OPTIONS*****************");
+							System.out.println("Press (1) to send an email\n" +
+											   "Press (2) to retrieve messages\n"+
+											   "Press (3) to return to authentication\n"+
+											   "Press (4) to exit.");
+							choice = Integer.parseInt(writer.readLine());
+							switch(choice)
+							{
+								case 1: //Gather user input for email structure
+									System.out.println("To: ");
+									receiver_email_address = writer.readLine();
+																		 
+									System.out.println("From: ");
+									sender_email_address = writer.readLine();
+									
+									System.out.println("Subject: ");
+									subject = writer.readLine();
+									
+									System.out.println("Message Body");
+									message_body = writer.readLine();
+									
+									MIME = ("From: "   + sender_email_address  + "\n"   +
+											"To: "     + receiver_email_address+ "\n"   +
+											"Subject: "+ subject                       + "\n"   +
+											"MIME-version: 1.0\n" +
+											"Content-Transfer-Encoding: 7bit\n" +
+											"Content-Type: text/plain\r\n\n" +
+											message_body);
+
+									out.println("SEND");
+									sendToServer();
+									break;
+									
+									
+								case 2:
+									break;
+								
+								case 3:
+									showOptions = false;
+									retry = true;
+									break;
+								
+								case 4:
+									showOptions = false;
+									break;
+								
+							}	
+							
+						}while(showOptions); 
+					}
+					
+		//***************Getting email**********************
 					
 				}while(retry);
 				
@@ -138,30 +224,12 @@ import java.lang.Math;
 	}
 
 	 /*
-	  *    Send authentication data to the server
+	  *    Send a message to the server
 	  *
 	  */
-	boolean sendAuthentication(String auth)
-	{
-			return false;
-	}
-
-	 /*
-	  *    Construct an email message using MIME. Alternatively, could push out one line at a time to the server so we can avoid parsing
-	  * 	a bunch of text.
-	  *
-	  */
-	void construct_EMAIL(String receiver_email_address, String sender_email_address, String  subject, String main_text){
+	void sendToServer(){
 		
-		MIME = ("From: "   + sender_email_address  + "\n"   +
-				"To: "     + receiver_email_address+ "\n"   +
-				"Subject: "+ subject			   + "\n"   +
-				"MIME-version: 1.0\n" +
-				"Content-Transfer-Encoding: 7bit\n" +
-				"Content-Type: text/plain\r\n\n" +
-				"Test Message."	
-			   );
-		
+	
 		String replyCode;
 		
 		while(state != SMTP_State.FINISH)
@@ -259,9 +327,9 @@ import java.lang.Math;
 					break;
 						
 				case FINISH: //Close the socket
-					socket.close();
-					out.close();
-					in.close();
+					//socket.close();
+					//out.close();
+					//in.close();
 					break;
 					
 				}
@@ -274,7 +342,7 @@ import java.lang.Math;
 		public static void main(String[] args)
 		{	
 				Client test = new Client();
-				//test.getUserInput();	
-				//test.construct_EMAIL(args[0], args[1], args[2], args[3]);
+				test.getUserInput();	
+				//test.sendMIME(args[0], args[1], args[2], args[3]);
 		}
 }
