7bd9d81b8af86d3580294121ef1c267295ba7fa9
pauledangerously1 <pauledangerously1@d6ec4651-b2ca-d279-b62f-cb9b843d6bd8>
2010-04-21 22:19:11 +0000

    Updated the subject parsing

--- src/Account.java
+++ src/Account.java
@@ -3,7 +3,11 @@
  * CNT4007 Project 2 
  */
 import java.util.ArrayList;
-
+import java.io.StringReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 //*******************************Account*******************************
 /*
@@ -60,13 +64,41 @@ import java.util.ArrayList;
 		 {
 			 this.mailNum = mailNum;
 			 this.message = message;
-			 subject = parse(message);
+			 subject = parseSubject();
 		 }
-		 //Fist latersc
-		 private String parse(String message)
+		 /*
+		  *	Extracts the subject line from the MIME formatted essage header 
+		  */
+		 private String parseSubject()
 		 {
-			 return message;
-		 }
+			String str = null;
+			BufferedReader reader = new BufferedReader(new StringReader(message));
+			try 
+			{
+			  while ((str = reader.readLine()) != null) 
+				if (str.contains("Subject: "))
+					return str;
+
+			} catch(IOException e) {
+			  e.printStackTrace();
+			}
+			return str;
+		}		
+		String getMessage()
+		{
+			return message;
+		}			 
+	 }
+	public static void main(String[] args)
+	 {
+		 Account parseTest = new Account("paul", "password");
+		 parseTest.addMail("To: Mark\nFrom: Paul\nSubject: Sillyface\nContent-Type: Whocares?");
+		 parseTest.addMail("To: Allison\nFrom: Paul\nSubject: Hello :3\nContent-Type: Whocares?");
+		 parseTest.addMail("To: Stephen\nFrom: Paul\nSubject: Hey mang\nContent-Type: Whocares?");
 		 
+		 for(int i = 0; i < parseTest.inbox.size(); i++)
+		 {
+			 System.out.println(parseTest.inbox.get(i).getMessage()+"\n");
+		 }
 	 }
 }
