6fdeee93a74fb54d112f2223b9e12f49e99f4994
markramasco <markramasco@d6ec4651-b2ca-d279-b62f-cb9b843d6bd8>
2010-04-21 05:06:33 +0000

    Multithreaded goodness!

---
+++ trunk/src/MultiServer.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+
+public class MultiServer {
+
+	/**
+	 *  Runs the main server
+	 */
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(-1);
+        }
+
+        while (listening)
+	    new ServerThread(serverSocket.accept()).start();
+
+        serverSocket.close();
+    }
+}
